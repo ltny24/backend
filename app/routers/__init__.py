519ed5a03e75f5c0d727f67ef75f5091a2ba04b3
Lê Thị Như Ý <ltny2424@clc.fitus.edu.vn>
2025-12-16 10:54:56 +0700

    Create init file and add profile router

---
+++ app/routers/__init__.py
@@ -0,0 +1,18 @@
+﻿# app/routers/__init__.py
+
+# Import tất cả các file router vào đây để main.py nhìn thấy
+from . import map_risk
+from . import ai_score
+from . import login_register
+from . import ai_hazard
+from . import rescue
+from . import live_data
+from . import system
+from . import alerts
+from . import user_alerts
+from . import forecast_7day
+from . import sos
+from . import past_hazards
+
+# --- QUAN TRỌNG: Thêm file mới vào đây ---
+from . import profile_data
